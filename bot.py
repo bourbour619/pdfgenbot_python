440f6ff058194318123956e400c9c8f80129416f
bourbour619 <bourbour619@gmail.com>
2021-05-23 14:06:31 +0430

    bot updated v1.2

--- bot.py
+++ bot.py
@@ -1,20 +1,16 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 from dotenv import load_dotenv
 import os 
 
 load_dotenv()
-
-bot_token = os.getenv('bot_token')
-updater = Updater(token=bot_token, use_context=True)
-dispatcher = updater.dispatcher
 
 import logging 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 
-def start(update, context):
+def start(update: Update, _: CallbackContext) -> None:
     # context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
     keyboard = [
         InlineKeyboardButton('راه اندازی مجدد', callback_data='1'),
@@ -25,7 +21,16 @@ def start(update, context):
     update.message.reply_text('چیکار کنیم رفیق ؟ ', reply_markup=reply_markup)
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def run_bot() -> None:
+    bot_token = os.getenv('bot_token')
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    run_bot()
