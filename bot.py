912ac5db401ca79a094ab131280a9042893d4655
bourbour619 <bourbour619@gmail.com>
2021-05-23 15:34:34 +0430

    bot updated v1.5

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import logging 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
+import logging
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 from dotenv import load_dotenv
 import os 
 
@@ -11,24 +10,25 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def start(update: Update, _: CallbackContext) -> None:
-    # context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    keyboard = [
-        [
-            InlineKeyboardButton('راه اندازی مجدد', callback_data='1'),
-            InlineKeyboardButton('ویرایش قبلی', callback_data='2'),
-        ],
-        [InlineKeyboardButton('بریم بعدی', callback_data='3')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('چیکار کنیم رفیق ؟ ', reply_markup=reply_markup)
+def start(update, context) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text='فایل یا فایل هاتو واسم بفرست ...')
 
+
+def file_handler(update, context):
+    file_id = update.message.document.file_id
+    newFile = context.bot.get_file(file_id)
+    if newFile:
+        newFile.download()
+        context.bot.send_message(chat_id=update.effective_chat.id, text='.فایلتو گرفتم')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='فایلت نیومده که ....!')
+        
 
 def run_bot() -> None:
     bot_token = os.getenv('bot_token')
-    updater = Updater(token=bot_token)
+    updater = Updater(token=bot_token, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('start', start))
-
+    updater.dispatcher.add_handler(MessageHandler(Filters.document | Filters.photo, 'file_handler'))
     updater.start_polling()
     updater.idle()
 
