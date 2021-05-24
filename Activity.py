c25e207c21e2bbcd27af944c236ac53b474abb2d
bourbour619 <bourbour619@gmail.com>
2021-05-24 10:11:10 +0430

    bot updated v1.11

---
+++ Activity.py
@@ -0,0 +1,22 @@
+from genericpath import exists
+import os
+from os import path
+
+class Activity :
+    def __init__(self, id) -> None:
+        self.id = id
+        self.root = path.join(path.join(path.dirname(__file__), 'files'), self.id)
+        if self.first() :
+            self.env()
+
+    def first(self) -> bool:
+        return not path.isdir(self.root)
+
+    def env(self) -> None:
+        os.mkdir(self.root)
+
+    def log(self) -> list:
+        return os.listdir(self.root)
+
+    def add(self, name) -> str:
+        return path.join(self.root, name)
--- bot.py
+++ bot.py
@@ -2,10 +2,13 @@ import logging
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 from dotenv import load_dotenv
 import os 
+import Activity
 
 from PIL import Image
 
 load_dotenv()
+
+activity = {}
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -14,15 +17,16 @@ logger = logging.getLogger(__name__)
 
 def start(update, context) -> None:
     context.bot.send_message(chat_id=update.effective_chat.id, text='فایل یا فایل هاتو واسم بفرست ...')
-
+    activity = Activity(id=update.effective_user.username)
+    
 
 def file_handler(update, context) -> None:
     fileDict = update.message.document or update.message.photo[-1]
     newFile = context.bot.get_file(fileDict.file_id)
     if newFile:
-        file_dir = os.path.join(os.path.dirname(__file__),'files')
-        newFile.download(os.path.join(file_dir, fileDict.file_name))
-        context.bot.send_message(chat_id=update.effective_chat.id, text='.فایلتو گرفتم')
+        file_dir = activity.add(name=fileDict.file_name or fileDict.file_id)
+        newFile.download(file_dir)
+        context.bot.send_message(chat_id=update.effective_chat.id, text='فایلتو گرفتم .')
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text='فایلت نیومده که ....!')
         
